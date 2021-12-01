20953387a176f860a58aca050d7333c82fac7b27
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-01 18:07:33 +0800

    [OOStepByStep] [joanne] feat: Practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,26 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+  private Integer klass = null;
+
+  public Teacher(String name, int age) {
+    super(name, age);
+  }
+
+  public Teacher(String name, int age, Integer klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  public int getKlass() {
+    return klass;
+  }
+
+  @Override
+  public String introduce() {
+    if (klass != null) {
+      return super.introduce() + String.format(" I am a Teacher. I teach Class %d.", klass);
+    }
+    return super.introduce() + " I am a Teacher. I teach No Class.";
+  }
 }
