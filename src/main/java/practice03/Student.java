817836a460b17e0fdcafdcac4d2eb90b943a2365
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-01 17:23:04 +0800

    [OOStepByStep] [joanne] feat: Practice03

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,21 @@
 package practice03;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+  private final int klass;
+
+  public Student(String name, int age, int klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  public int getKlass() {
+    return klass;
+  }
+
+  @Override
+  public String introduce() {
+    return String.format("I am a Student. I am at Class %d.", klass);
+  }
 }
