628e46ef5a5ece4599a5a8ede408a9a9d91bf74b
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-01 17:12:29 +0800

    [OOStepByStep] [joanne] feat: done Practice01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -15,4 +15,8 @@ public class Person {
     public int getAge() {
         return age;
     }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
 }
