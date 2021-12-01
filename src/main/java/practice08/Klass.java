eac0981724d0ea65fe9e6130c000770190b11e71
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-01 19:09:41 +0800

    [OOStepByStep] [joanne] feat: Practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,32 @@
 package practice08;
 
 public class Klass {
+  private final int number;
+  private Student leader = null;
+
+  public Klass(int number) {
+    this.number = number;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public String getDisplayName() {
+    return String.format("Class %d", number);
+  }
+
+  public void assignLeader(Student student) {
+    if (student.getKlass().getNumber() == number) {
+      this.leader = student;
+    } else System.out.print("It is not one of us.\n");
+  }
+
+  public Student getLeader() {
+    return leader;
+  }
+
+  public void appendMember(Student student) {
+    student.setKlass(this);
+  }
 }
